e6222e053b4764d6523fe73a1f43b500d4c355da
kowloon-dev <kowloon-dev@kowloonet.org>
2018-09-02 23:39:09 +0900

    switch1 時刻計算のロジック追加

--- switch_1.py
+++ switch_1.py
@@ -2,6 +2,8 @@
 
 import wiringpi as pi
 import time
+import datetime
+
 
 SW_PIN = 4
 
@@ -9,10 +11,55 @@ pi.wiringPiSetupGpio()
 pi.pinMode( SW_PIN, pi.INPUT )
 pi.pullUpDnControl( SW_PIN, pi.PUD_DOWN )
 
-while True:
+#while True:
+    # if ( pi.digitalRead( SW_PIN ) == pi.HIGH ):
+    #     print("Switch is On")
+    #     # 通電を検知したら、ループを抜ける
+    #     break
+    # else:
+    #     print("Switch is Off")
+
+
+start_time_dt = datetime.datetime.now()
+print("start_time : " + str(start_time_dt))
+
+for i in range(4000):
     if ( pi.digitalRead( SW_PIN ) == pi.HIGH ):
         print("Switch is On")
-        # 通電を検知したら、ループを抜ける
+        hit_time_dt = datetime.datetime.now()
+        print("hit_time :" + str(hit_time_dt))
+        
+        # 通電を検知したら、「的に命中した」と判断し、所要時間の計算ロジック開始
+        #
+        # 1. 時刻の差分を計算
+        #    "的にヒットした時刻" から "ブザーが鳴った時刻(開始時刻)" を減算してデルタ時間を求める
+        delta = hit_time_dt - start_time_dt
+
+        # 2. デルタ時間を"秒.マイクロ秒"の形式にする
+        result_sec_microsec = str(delta.seconds) + "." + str(delta.microseconds)
+
+        print(result_sec_microsec)
+
+        # 3. 小数点以下2位(100分の1秒単位)より下を切り捨てる
+        #    (ショットタイマーに使うLED液晶が4桁表示なので、収まるようにするための措置)
+        #    round()で丸めると、小数点以下第2位が0のときに位自体が削られてしまうため
+        #    あえてスライスを使い、おしりの方から数えて4桁までを削っている
+        result = result_sec_microsec[:-4]
+        
+        # 命中時の処理終わり
+        # forループを抜ける
         break
     else:
-        print("Switch is Off")
+        # GPIOピンでHIGHを検知できない = 通電していない = まだ的に命中していない
+        # と判断し、1000分の1秒スリープしてからチェックに戻る
+        time.sleep(0.001)
+else:
+    # 指定した締め切り時間内にヒットを検知しなかったので、
+    # 失敗(的を外した or 締め切り時間内に撃ち込めなかった)と見なし、forループを終える
+    print("タイムオーバー！")
+    
+    # 戻り値にfailと代入
+    result = "FAIL"
+
+print(result)
+
